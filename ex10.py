2854b8bfe4dbf3e7e6db6fb7647d61322fb38688
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2024-02-11 19:08:22 +0200

    (Try Escape Sequences) Complete

---
+++ ex10.py
@@ -0,0 +1,13 @@
+# Escape Sequences
+varMultipleEscapeSequences = "This\tis\tmy\tfirst\tline\nAnd this is a second line. \'day \\ 10\'"
+
+# Print the varMultipleEscapeSequences using f-string
+print(f'Using Multiple Escape Sequences \n{varMultipleEscapeSequences}')
+
+# Print a long comment using a triple single-quote  
+print('''
+A List Of Programming:
+    \t*\tPython
+    \t*\tJava
+    \t*\tC++
+''')
